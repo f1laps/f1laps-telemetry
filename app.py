34d7d50ce763f5da47ae2e69b57f377a5f45fab6
F1Laps <contact@f1laps.com>
2021-02-28 13:13:16 +0100

    Test error on main thread

--- app.py
+++ app.py
@@ -3,13 +3,13 @@ from pathlib import Path
 import sys
 import os
 
+from lib.logger import log
 from gui.window import MainWindow
 from gui.styles import GUI_STYLES
-from lib.logger import log
 
 
-if __name__ == '__main__':
-    try:
+try:
+    if __name__ == '__main__':
         # Create App
         app = QApplication(sys.argv)
         app.setStyleSheet(GUI_STYLES)
@@ -17,10 +17,10 @@ if __name__ == '__main__':
         # Create & show UI window
         window = MainWindow()
         window.show()
+        raise OSError('Emulated exception to be traced back to')
 
         # Handle window close
         exit_code = app.exec_()
         sys.exit(exit_code)
-    except Exception as ex:
-        log.error("Encountered exception %s on main thread" % ex, exc_info=True)
-        raise Exception
+except Exception as ex:
+    log.error("Encountered exception %s on main thread" % ex, exc_info=True)
