b618ed8da5350f26ddbec100f8ebeaa8bb50aa72
F1Laps <contact@f1laps.com>
2021-03-03 10:15:37 +0100

    Delete pat.py

--- pat.py
+++
@@ -1,7 +0,0 @@
-import os
-from os import path
-
-os_path = os.path.dirname(os.path.abspath(__file__))
-path_to_dat = path.abspath(path.join(path.dirname(__file__), 'file.dat'))
-
-print(path_to_dat)
