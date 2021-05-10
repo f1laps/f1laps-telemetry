d0634396e5d6beb041f327784eca62f01dde7c51
F1Laps <contact@f1laps.com>
2021-05-10 19:51:40 +0200

    Add validation and multithreading

--- gui/window.py
+++ gui/window.py
@@ -1,7 +1,6 @@
 from PyQt5.QtWidgets import QWidget, QLabel, QPushButton, QLineEdit, \
                             QVBoxLayout, QFrame
-from PyQt5.QtCore import Qt
-#from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import Qt, QObject, QThread, pyqtSignal
 from PyQt5.QtSvg import QSvgWidget
 import logging
 import requests
@@ -11,6 +10,9 @@ from lib.file_handler import ConfigFile, get_path_temporary
 from receiver.receiver import RaceReceiver
 from receiver.helpers import get_local_ip
 import config
+
+F1LAPS_VERSION_ENDPOINT = "https://www.f1laps.com/api/f12020/telemetry/app/version/current/"
+F1LAPS_USER_SETTINGS_ENDPOINT = "https://www.f1laps.com/api/f12020/telemetry/app/user/settings/"
 
 
 class QHSeperationLine(QFrame):
@@ -22,6 +24,7 @@ class QHSeperationLine(QFrame):
         self.setFrameShape(QFrame.HLine)
         self.setFrameShadow(QFrame.Sunken)
 
+
 class QVSpacer(QLabel):
     """ Vertical empty space """
     def __init__(self, height):
@@ -30,13 +33,105 @@ class QVSpacer(QLabel):
         self.setFixedHeight(height)
 
 
-class MainWindow(QWidget):
+class StartButton(QPushButton):
+    """ Main button to start/stop telemetry receiver"""
+    def __init__(self):
+        super().__init__('Start Telemetry')
+        self.setObjectName("startButton")
+        self.setFixedSize(160, 45)
+
+    def set_validating_api_key(self):
+        self.setText("Starting...")
+
+    def set_running(self):
+        self.setText("Stop Telemetry")
+        self.setStyleSheet("background-color: #B91C1C;")
+
+    def reset(self):
+        self.setText("Start Telemetry")
+        self.setStyleSheet("background-color: #4338CA;")
+
+
+class StatusLabel(QLabel):
+    """ Text below StartButton to display current receiver status """
+    def __init__(self):
+        super().__init__()
+        self.setText("Status: not started")
+        self.setObjectName("statusLabel")
+        self.setFixedSize(100, 15)
+
+    def set_validating_api_key(self):
+        self.setText("Checking API key...")
     
+    def set_running(self):
+        self.setText("Status: running")
+
+    def reset(self):
+        self.setText("Status: not started")
+
+
+class TelemetrySession:
+    def __init__(self):
+        # The actual receiver session
+        self.session = None
+        self.is_active = False
+
+    def start(self, api_key):
+        receiver_thread = RaceReceiver(api_key)
+        receiver_thread.start()
+        self.is_active = True
+        log.info("Started receiver tread")
+        return True
+
+    def kill(self):
+        self.session.kill()
+        self.is_active = False
+        log.info("Stopped receiver tread")
+        return True
+
+
+class Worker(QObject):
+    user_settings = pyqtSignal(dict)
+    finished = pyqtSignal()
+
+    def __init__(self, api_key):
+        super().__init__()
+        self.api_key = api_key
+
+    def run(self):
+        """ Return if user is authenticated and supports telemetry 
+        (user_is_valid, user_telemetry_enabled)"""
+        user_settings_dict = {
+            "api_key_valid": False,
+            "telemetry_enabled": False
+        }
+        try:
+            headers = {'Authorization': 'Token %s' % self.api_key,}
+            response = requests.get(F1LAPS_USER_SETTINGS_ENDPOINT, headers=headers)
+            if response.status_code == 401:
+                log.info("API key %s is invalid" % self.api_key)
+                self.user_settings.emit(user_settings_dict)
+            elif response.status_code == 200:
+                telemetry_enabled = json.loads(response.content).get('telemetry_enabled')
+                log.info("Authenticated successfully. Telemetry is %s" % \
+                    ("enabled" if telemetry_enabled else "not enabled"))
+                user_settings_dict["api_key_valid"] = True
+                user_settings_dict["telemetry_enabled"] = telemetry_enabled
+                self.user_settings.emit(user_settings_dict)
+            else:
+                log.warning("Received invalid F1Laps status code %s" % response.status_code)
+                self.user_settings.emit(user_settings_dict)
+        except Exception as ex:
+            log.warning("Couldn't get user settings from F1Laps due to: %s" % ex)
+            self.user_settings.emit(user_settings_dict)
+        self.finished.emit()
+
+
+class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
         self.api_key_field = None
         self.api_key = ConfigFile().get_api_key()
-        self.start_button = None
         self.app_version = config.VERSION
 
         # Draw the window UI
@@ -49,9 +144,7 @@ class MainWindow(QWidget):
         self.check_version()
 
         # Track if we have an active receiver
-        # A receiver process can only run once
-        self.session = None
-
+        self.session = TelemetrySession()
 
     def init_ui(self):
         # 1) Logo & heading
@@ -83,14 +176,9 @@ class MainWindow(QWidget):
         self.ip_value.setContentsMargins(0, 5, 0, 0)
 
         # Start/Stop button
-        self.start_button = QPushButton('Start Telemetry')
-        self.start_button.setObjectName("startButton")
+        self.start_button = StartButton()
         self.start_button.clicked.connect(lambda: self.start_button_click())
-        self.start_button.setFixedSize(160, 45)
-        self.status_label = QLabel()
-        self.status_label.setText("Status: not started")
-        self.status_label.setObjectName("statusLabel")
-        self.status_label.setFixedSize(100, 15)
+        self.status_label = StatusLabel()
 
         help_text_label = QLabel()
         help_text_label.setText("Need help? <a href='https://www.notion.so/F1Laps-Telemetry-Documentation-55ad605471624066aa67bdd45543eaf7'>Check out the Documentation & Help Center!</a>")
@@ -139,10 +227,8 @@ class MainWindow(QWidget):
         self.setWindowTitle("F1Laps Telemetry") 
         log.info("Welcome to F1Laps Telemetry! You will see all logging in this text field.")
 
-
     def set_ip(self):
         self.ip_value.setText(get_local_ip())
-
 
     def get_api_key(self):
         """ Get API key from user input field and store it in local file """
@@ -150,10 +236,9 @@ class MainWindow(QWidget):
         ConfigFile().set_api_key(api_key)
         return api_key
 
-
     def check_version(self):
         try:
-            response = requests.get("https://www.f1laps.com/api/f12020/telemetry/app/version/current/")
+            response = requests.get(F1LAPS_VERSION_ENDPOINT)
             version = response.json()['version']
             user_version_int = int(self.app_version.replace(".", ""))
             current_version_int = int(version.replace(".", ""))
@@ -166,14 +251,10 @@ class MainWindow(QWidget):
         except Exception as ex:
             log.warning("Couldn't get most recent version from F1Laps due to: %s" % ex)
 
-
     def start_button_click(self):
-        if not self.session:
+        if not self.session.is_active:
             log.info("Starting new session")
-            self.session = self.start_telemetry()
-            self.start_button.setText("Stop Telemetry")
-            self.start_button.setStyleSheet("background-color: #B91C1C;")
-            self.status_label.setText("Status: running")
+            self.start_telemetry()
         else:
             log.info("Stopping session")
             self.session = self.stop_telemetry()
@@ -181,25 +262,58 @@ class MainWindow(QWidget):
             self.start_button.setStyleSheet("background-color: #4338CA;")
             self.status_label.setText("Status: stopped")
 
-
     def start_telemetry(self):
-        if self.session:
+        if self.session.is_active:
             log.error("A new session can't be started when another one is active")
-            return self.session
+            return False
         # Get API key from input field
         api_key = self.get_api_key()
-        # Start receiver thread
-        receiver_thread = RaceReceiver(api_key)
-        receiver_thread.start()
-        log.debug("Session initiated and started")
-        return receiver_thread
+        # Validate API key via F1Laps API
+        self.validate_api_key(api_key)
+        """if not api_key_valid:
+            log.info("Session not started because API key is invalid")
+            return False
+        
+        """
+
+    def validate_api_key(self, api_key):
+        # Create a QThread object
+        self.thread = QThread()
+        # Create the worker object
+        self.worker = Worker(api_key)
+        # Move worker to the thread
+        self.worker.moveToThread(self.thread)
+        # Connect signals and slots
+        self.thread.started.connect(self.worker.run)
+        self.worker.finished.connect(self.thread.quit)
+        self.worker.finished.connect(self.worker.deleteLater)
+        self.thread.finished.connect(self.thread.deleteLater)
+        self.worker.user_settings.connect(self.set_user_settings)
+        # Start the thread
+        self.thread.start()
+        # Set buttons to loading state
+        self.start_button.set_validating_api_key()
+        self.status_label.set_validating_api_key()
+
+    def set_user_settings(self, user_settings_dict):
+        api_key_valid = user_settings_dict.get("api_key_valid")
+        telemetry_enabled = user_settings_dict.get("telemetry_enabled")
+        if api_key_valid:
+            log.info("Starting Telemetry session")
+            self.start_button.set_running()
+            self.status_label.set_running()
+            # Actually start receiver thread
+            self.session.start(api_key)
+        else:
+            log.info("Not starting Telemetry session")
+            self.start_button.reset()
+            self.status_label.reset()
 
 
     def stop_telemetry(self):
-        if not self.session:
+        if not self.session.is_active:
             log.error("Session can't be stopped as there is no active session")
             return None
         self.session.kill()
         log.debug("Session has been stopped")
-        self.session = None
         return None
